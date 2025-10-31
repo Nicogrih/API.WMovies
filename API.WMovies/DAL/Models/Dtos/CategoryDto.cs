0a0c7f5d15989615b0a416741c10df6828bd4590
Nicolas <nicolasgrijalba29@gmail.com>
2025-10-31 17:49:14 -0500

    AutoMapper

---
+++ API.WMovies/DAL/Models/Dtos/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace API.WMovies.DAL.Models.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ API.WMovies/MoviesMapper/Mappers.cs
@@ -0,0 +1,15 @@
+﻿using API.WMovies.DAL.Models;
+using API.WMovies.DAL.Models.Dtos;
+using AutoMapper;
+
+namespace API.WMovies.MoviesMapper
+{
+    public class Mappers: Profile
+    {
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+        }
+    }
+}
