5d7355fd703c8238fa525057767e8cbfc829e73c
Nicolas <nicolasgrijalba29@gmail.com>
2025-10-31 17:31:56 -0500

    CategoryRepository created

--- API.WMovies/DAL/Models/Category.cs
+++ API.WMovies/DAL/Models/Category.cs
@@ -4,6 +4,8 @@ namespace API.WMovies.DAL.Models
 {
     public class Category:AuditBase
     {
+        internal DateTime UpdatedDate;
+
         [Required]
         public string Name { get; set; }
     }
---
+++ API.WMovies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using API.WMovies.DAL.Models;
+using System.Collections.ObjectModel;
+
+namespace API.WMovies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>>GetCategoriesAsync();//retorna una lista de categorias
+        Task<Category>GetCategoryAsync(int id);//Retorna una categoria por id
+        Task<bool> CategoryExistsbyidAsync(int id);
+        Task<bool>CategoryExistsbyNameAsync(string name);
+        Task<bool> CreateCategoryAsync(Category category);
+        Task<bool> UpdateCategoryAsync(Category category);
+        Task<bool> DeleteCategoryAsync(int id);
+
+
+    }
+}
